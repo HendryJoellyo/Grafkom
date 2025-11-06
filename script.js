da434ac48204087ac6d2ecd0fe801bf558c0ace0
HendryJoellyo <hendryjoel08@gmail.com>
2025-11-06 18:52:02 +0700

    fitur animasi min & sort

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("arrayCanvas");
 const ctx = canvas.getContext("2d");
 
 let animating = false; // ini buat mencegah animasi lain berjalan ketika animasi sedang berjalan
+let items = []; // array untuk menyimpan nilai-nilai
 
 function translasi(titik_lama, T) {
   return { x: titik_lama.x + T.x, y: titik_lama.y + T.y }; // fungsi pergeseran
@@ -53,17 +54,6 @@ function drawBox2(value, index, x, y, alpha = 1) { // fungsi untuk menggambar ko
   ctx.globalAlpha = 1;
 }
 
-function drawArray() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
-  const boxWidth = 50;
-  const boxHeight = 30;
-  let x = 13; // titik horizontal
-
-  items.forEach((value, index) => { // untuk setiap value dan index digambar kotak biru
-    drawBox(value, index, x, 50);
-    x += boxWidth + 2; // gap kedua kotak
-  });
-}
 
 function animateArrayCreation() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -119,9 +109,11 @@ function createArray() {
   for (let i = 0; i < n; i++) { 
     items[i] = Math.floor(Math.random() * 201) -100; // ini untuk ngerandom value di dalam array
   }
+  items.sort((a, b) => a - b);
 
   animating = true; // signal untuk memberi tau bahwa animasi sedang berjalan
   animateArrayCreation();
+  return items;
 }
 
 
@@ -204,19 +196,18 @@ function searchValue() {
   }
 }
 
+
 function minValue() {
+  let index = 0;
+  let pos = { x: 13, y: 50 };
+
   if (items.length === 0) {
     alert("Array kosong");
     return;
   }
   else{
-    min = items[0];
-    for (let i = 1; i < items.length; i++) {
-      if (items[i] < min) {
-        min= items[i];
-      }
-    }
-    alert("Nilai minimum adalah: " + min);
+    let x = Math.min(...items);
+    drawBox2(x, index, pos.x, pos.y);
   }
 }
   
